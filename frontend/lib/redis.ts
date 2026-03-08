4c5d2afd0817cf92a83b91930f63abe9de8fb4ce
TSOA2 <zachary@gadad.net>
2026-03-07 18:03:47 -0800

    redis use envs

--- frontend/lib/redis.ts
+++ frontend/lib/redis.ts
@@ -1,12 +1,3 @@
 import { Redis } from '@upstash/redis'
 
-// Prefer env vars (required for Vercel). Fallback for local dev only.
-const url = process.env.UPSTASH_REDIS_REST_URL
-const token = process.env.UPSTASH_REDIS_REST_TOKEN
-
-export const redis = new Redis({
-  url: url || '',
-  token:
-    token ||
-    '',
-})
+export const redis = Redis.fromEnv()
